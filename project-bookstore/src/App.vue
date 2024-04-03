3212bab0994323f9c027116ec1bb91b56c848809
Mari Astamadze <marihamburg321@gmail.com>
2024-04-03 19:55:37 +0200

    add booklist component wit styling and bokks data

--- project-bookstore/src/App.vue
+++ project-bookstore/src/App.vue
@@ -1,13 +1,17 @@
 <template>
   <LayoutHeader />
+  <BookList />
 </template>
 
 <script>
+import BookList from "./components/BookList.vue";
 import LayoutHeader from "./components/LayoutHeader.vue";
+
 export default {
   name: "App",
   components: {
     LayoutHeader,
+    BookList,
   },
 };
 </script>
