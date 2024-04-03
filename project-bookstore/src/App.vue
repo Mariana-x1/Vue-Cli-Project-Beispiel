e84bd86063f47763cb3860dad15967c82bc04970
Mari Astamadze <marihamburg321@gmail.com>
2024-04-03 19:34:11 +0200

    add loho and LayoutHeader component with Titel

--- project-bookstore/src/App.vue
+++ project-bookstore/src/App.vue
@@ -1,5 +1,13 @@
 <template>
-  <h1>Hello Project-BookStore</h1>
+  <LayoutHeader />
 </template>
 
-<script></script>
+<script>
+import LayoutHeader from "./components/LayoutHeader.vue";
+export default {
+  name: "App",
+  components: {
+    LayoutHeader,
+  },
+};
+</script>
---
+++ project-bookstore/src/components/LayoutHeader.vue
@@ -0,0 +1,40 @@
+<template>
+  <header class="header">
+    <div class="logo-container">
+      <img src="@/assets/logo.jpg" alt="Logo" />
+      <h1 class="project-name">Hello Project-BookStore</h1>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "LayoutHeader",
+};
+</script>
+
+<style scoped>
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem;
+  background-color: #333666;
+  color: #fff;
+}
+
+.logo-container {
+  display: grid;
+  grid-template-columns: auto auto;
+  align-items: center;
+  margin-left: 4rem;
+}
+.logo-container img {
+  height: 3rem;
+}
+.project-name {
+  color: #998111;
+  margin-left: 4rem;
+  font-size: 1.5rem;
+}
+</style>
